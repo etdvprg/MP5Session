195ea0a50dc47cd72dc02d25d1663ec003e8d8e8
etdvprg <encetabuzo@gmail.com>
2023-11-24 16:16:13 +0800

    UPDATE: (draft) integration of the questions and the gameplay (very complicated)

--- src/main/java/GameplayServlet.java
+++ src/main/java/GameplayServlet.java
@@ -7,6 +7,12 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class GameplayServlet extends HttpServlet {
+    
+    /*
+    private int score = 0;
+    private int questCtr = 0;
+    private QuestionBank qBank;
+    */
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -21,6 +27,40 @@ public class GameplayServlet extends HttpServlet {
         //Store user session
         HttpSession session = request.getSession();
         session.setAttribute("username", gamertag);
-        response.sendRedirect("/MP5Session/ready.jsp");
+        
+        if (gamertag != null) {
+            response.sendRedirect("/MP5Session/ready.jsp");
+        } // should have some mechanisms placed here for null usernames
+        
+        
+        
+       /* String answer = request.getParameter("answer");
+        String correctAnswer = qBank.getQuestion(request.getParameter("difficulty"), questCtr).getSolutions().get(qBank.getQuestion(request.getParameter("difficulty"), questCtr).getCorrectSolIndex());
+    
+        if (answer.equals(correctAnswer)) {
+            score++;
+        }
+        
+        questCtr++;
+        
+        if (questCtr < 10 && answer.equals(correctAnswer)) {
+            // Move to the next question
+            request.setAttribute("score", score);
+            request.setAttribute("question", questionBank.getQuestion(request.getParameter("difficulty"), questionNumber));
+            request.setAttribute("difficulty", request.getParameter("difficulty"));
+            request.getRequestDispatcher("/gameshow.jsp").forward(request, response);
+        } else if (questionNumber == 10 && answer.equals(correctAnswer)) {
+            // Player has won
+            request.setAttribute("score", score);
+            response.sendRedirect("/MP5Session/aftermath.jsp");
+        } else {
+            // Game over
+            request.setAttribute("score", score);
+            response.sendRedirect("/MP5Session/gameover.jsp");
+        }
+        */
     }
+        
+    
 }
+
