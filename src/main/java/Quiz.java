ed837d6f355a9e3a4fefcd910e92d032d2746bd7
etdvprg <encetabuzo@gmail.com>
2023-11-27 04:59:19 +0800

    ADD: Quiz.java, would facilitate the trivia (helper class) (BETA)

---
+++ src/main/java/Quiz.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Quiz {
+    public List<Question> questions;
+    private int currentQuestionIndex;
+    
+    public Quiz(List<Question> questions) {
+        this.questions = questions;
+        this.currentQuestionIndex = 0;
+    }
+    
+    public Question getQuestion() {
+        return questions.get(currentQuestionIndex);
+    }
+    
+    public boolean isQuizComplete() {
+        return currentQuestionIndex > questions.size(); //could be 9 if hard-coded
+    }
+    
+    public void nextQuestion() {
+        currentQuestionIndex++;
+    }
+}
