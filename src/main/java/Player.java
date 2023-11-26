1bab48ae73039914f367767b865444931de8c43c
etdvprg <encetabuzo@gmail.com>
2023-11-27 04:57:49 +0800

    ADD: Player.java (helper class)

---
+++ src/main/java/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+
+    private String username;
+    private int score;
+    private long timer;
+    private boolean gameStatus;
+
+    public Player(String username) {
+        this.username = username;
+        this.score = 0;
+        this.timer = System.currentTimeMillis();
+        this.gameStatus = false;
+    }
+
+    public void answerQuestion(String answer, Question question) {
+        long elapsedTime = System.currentTimeMillis() - this.timer;
+
+        this.score += 1000 / elapsedTime;
+        this.timer = System.currentTimeMillis();
+
+        //if answer is false, then gameStatus = true;
+    }
+    
+    //more methods to come
+
+}
