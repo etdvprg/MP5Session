bee0bd0769380775e40b493214adc32a81acae59
etdvprg <encetabuzo@gmail.com>
2023-11-27 05:00:57 +0800

    ADD: QuizManager.java, mainly handles the randomization of the quiz (helper class) (BETA)

---
+++ src/main/java/QuizManager.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class QuizManager {
+    private List<Question> allQuestions;
+    private List<Question> easyQuestions;
+    private List<Question> intermediateQuestions;
+    private List<Question> difficultQuestions;
+
+    public QuizManager() {
+        this.allQuestions = new ArrayList<>();
+        this.easyQuestions = new ArrayList<>();
+        this.intermediateQuestions = new ArrayList<>();
+        this.difficultQuestions = new ArrayList<>();
+    }
+
+    public void addQuestion(String quest, List<String> possibleAnswers, int correctSolIndex, Difficulty difficulty) {
+        Question question = new Question(quest, possibleAnswers, correctSolIndex, difficulty);
+        allQuestions.add(question);
+        sortQuestion(question);
+    }
+    
+    public void sortQuestion(Question question) {
+        switch (question.getDifficulty()) {
+            case EASY:
+                easyQuestions.add(question);
+                break;
+            case INTERMEDIATE:
+                intermediateQuestions.add(question);
+                break;
+            case DIFFICULT:
+                difficultQuestions.add(question);
+                break;
+        }
+    }
+
+    public Quiz createQuiz() {
+        List<Question> randomizedQuestionSet = new ArrayList<Question>();
+
+        Collections.shuffle(easyQuestions);
+        Collections.shuffle(intermediateQuestions);
+        Collections.shuffle(difficultQuestions);
+
+        randomizedQuestionSet.addAll(easyQuestions.subList(0, 3));
+        randomizedQuestionSet.addAll(intermediateQuestions.subList(0, 3));
+        randomizedQuestionSet.addAll(difficultQuestions.subList(0, 4));
+
+        return new Quiz(randomizedQuestionSet);
+    }
+}
