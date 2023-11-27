663f3a766be32020bbabb0d6e70c69c03273bdd2
etdvprg <encetabuzo@gmail.com>
2023-11-27 23:09:41 +0800

    ADD: new logic, update for the initialization of the web application

---
+++ src/main/java/com/AuthenticationServlet.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthenticationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String username = request.getParameter("username");
+
+        if (username != null) {
+            Player player = new Player(username);
+            session.setAttribute("player", player);
+            response.sendRedirect("/MP5Session/ready.jsp");
+        }
+    }
+}
