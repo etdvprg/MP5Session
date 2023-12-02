072b869a1469c467766e1fdaa2047d7d9aab43fe
Enzoe <74560055+etdvprg@users.noreply.github.com>
2023-12-02 14:46:26 +0800

    UPDATE: AdminServlet.java (remote)

--- src/main/java/com/AdminServlet.java
+++ src/main/java/com/AdminServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AdminServlet extends HttpServlet {
-    
+    @Override
     protected void doGet (HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
